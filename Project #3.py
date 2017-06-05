3e443ca762d13d6f1e8b047a541b1565007871cf
Nick Cohron <dcohron@stevens.edu>
2017-06-04 20:57:32 -0400

    Update

--- Project #3.py
+++ Project #3.py
@@ -8,7 +8,7 @@
 # CodeOutputProject#3.txt     Output to terminal from this program and input file above
 
 # Honor Code Statement
-# We pledge on my honor that we have not given or received any 
+# We pledge on our honor that we have not given or received any 
 # unauthorized assistance on this assignment/examination. 
 # We further pledge that we have not copied any material from 
 # a book, article, the Internet or any other source except 
@@ -19,51 +19,73 @@
 
 # References:
 # 1) StackOverflow
-# 2) http://wiki-en.genealogy.net/GEDCOM-Tags
+# 2) GEDCOM tag full list:  http://wiki-en.genealogy.net/GEDCOM-Tags
+# 3) Error Handling:  http://www.pythonforbeginners.com/error-handling/python-try-and-except
 
 
-## Import needed libraries
-import re
 
-# Define where to find the data file
-path1 = "/Users/Nick/Stevens Institute of Technology/Agile Methods/Homework/My-Family-18-May-2017-411.ged"
-path2 = "/Users/Nick/Stevens Institute of Technology/Agile Methods/Homework/Homework2Test.ged"
-path = path1
+# put code into try/except for error handling
+try: 
 
-# List of valid GEDCOM tags
-tagLong = ["ABBR", "ADDR", "ADR1", "ADR2", "ADOP", "AFN", "AGE", "AGNC", "ALIA", "ANCE", "ANCI", "ANUL", "ASSO", "AUTH", "BAPL", "BAPM", "BARM", "BASM", "BIRT", "BLES", "BLOB", "BURI", "CALN", "CAST", "CAUS", "CENS", "CHAN", "CHAR", "CHIL", "SHR", "CHRA", "CITY", "CONC", "CONF", "CONL", "CONT", "COPR", "CORP", "CREM", "CTRY", "DATA", "DATE", "DEAT", "DESC", "DESI", "DEST", "DIV", "DIVF", "DSCR", "EDUC", "EMAIL", "EMIG", "ENDL", "ENGA", "EVEN", "FACT", "FAM", "FAMC", "FAMF", "FAMS", "FAX", "FCOM", "FILE", "FONE", "FORM", "GEDC", "GIVN", "GRAD", "HEAD", "HUSB", "IDNO", "IMMI", "INDI", "LANG", "LATI", "LEGA", "LONG", "MAP", "MARB", "MARC", "MARL", "MARR", "MARS", "MEDI", "NAME", "NATI", "NATU", "NCHI", "NICK", "NMR", "NOTE", "NPFX", "NSFX", "OBJE", "OCCU", "ORDI", "ORDN", "PAGE", "PEDI", "PHON", "PLAC", "POST", "PROB", "PROP", "PUBL", "QUAY", "REFN", "RELA", "RELI", "REPO", "RESI", "RESN", "RETI", "RFN", "RIN", "ROLE", "ROMN", "SEX", "SLGC", "SLGS", "SOUR", "SPFX", "SSN", "STAE", "STAT", "SUBM", "SUBN", "SURN", "TEMP", "TEXT", "TIME", "TITL", "TRLR", "TYPE", "VERS", "WIFE", "WWW", "WILL"]
-tagShort = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
-tagList = tagShort
+	## Import needed libraries
+	import re
 
-## Read in the file line by line
-# It is important to process file line by line because 
-# we do not know how long the file is
-with open(path) as fp:
+	# Define where to find the data file
+	path1 = "/Users/Nick/Stevens Institute of Technology/Agile Methods/Project/AgileMethods/My-Family-18-May-2017-411.ged"
+	path2 = ""
+	path = path1
+
+	# List of valid GEDCOM tags
+	tagLong = ["ABBR", "ADDR", "ADR1", "ADR2", "ADOP", "AFN", "AGE", "AGNC", "ALIA", "ANCE", "ANCI", "ANUL", "ASSO", "AUTH", "BAPL", "BAPM", "BARM", "BASM", "BIRT", "BLES", "BLOB", "BURI", "CALN", "CAST", "CAUS", "CENS", "CHAN", "CHAR", "CHIL", "SHR", "CHRA", "CITY", "CONC", "CONF", "CONL", "CONT", "COPR", "CORP", "CREM", "CTRY", "DATA", "DATE", "DEAT", "DESC", "DESI", "DEST", "DIV", "DIVF", "DSCR", "EDUC", "EMAIL", "EMIG", "ENDL", "ENGA", "EVEN", "FACT", "FAM", "FAMC", "FAMF", "FAMS", "FAX", "FCOM", "FILE", "FONE", "FORM", "GEDC", "GIVN", "GRAD", "HEAD", "HUSB", "IDNO", "IMMI", "INDI", "LANG", "LATI", "LEGA", "LONG", "MAP", "MARB", "MARC", "MARL", "MARR", "MARS", "MEDI", "NAME", "NATI", "NATU", "NCHI", "NICK", "NMR", "NOTE", "NPFX", "NSFX", "OBJE", "OCCU", "ORDI", "ORDN", "PAGE", "PEDI", "PHON", "PLAC", "POST", "PROB", "PROP", "PUBL", "QUAY", "REFN", "RELA", "RELI", "REPO", "RESI", "RESN", "RETI", "RFN", "RIN", "ROLE", "ROMN", "SEX", "SLGC", "SLGS", "SOUR", "SPFX", "SSN", "STAE", "STAT", "SUBM", "SUBN", "SURN", "TEMP", "TEXT", "TIME", "TITL", "TRLR", "TYPE", "VERS", "WIFE", "WWW", "WILL"]
+	tagShort = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
+	tagInterest = ["NAME", "SEX", "BIRT", "DEAT", "CHIL", "HUSB", "WIFE", "MARR"]
+	tagList = tagInterest
+	
+
+	with open(path) as fp:
+		# declare empty dictionary as primary data structure
+		individuals = {}
+
+		## Read in the file line by line
+		# It is important to process file line by line because 
+		# we do not know how long the file is
 		for line in fp:
 			# skips leading comments and any line that does not 
 			# start with a number
 			if line[0].isdigit():
 				# strip trailing whitespace
 				line = line.rstrip()
-				print("-->", line)
+				
 				# split line into words
 				wordList = line.split()
-				# print (wordList)
+				print (wordList)
 				# print(len(wordList))
 				remainder = ''.join(str(w + " ") for w in wordList[2:])
-				# if word is in the list of tags, print it
-				if wordList[1] in tagList:
-					print("<-- ", wordList[0], "|", wordList[1], "|Y|", remainder, sep ='')
-				# if tag in word 3 is two special tags, print it	
+				# if word is in the list of tags, add to dictionary
+				if wordList[0] == '1' and wordList[1] in tagList:
+					individuals[id][wordList[1]] = wordList[2]
+					
+				# if tag in word 3 is one of two special tags, handle it	
 				elif len(wordList) > 2 and (wordList[2] == "INDI" or wordList[2] == "FAM"):
-					print("<-- ", wordList[0], "|", wordList[2], "|Y|", wordList[1], sep ='')
-				# # if tag begins with _ it is a custom tag, print it
-				# elif wordList[1].startswith("_"):
-				# 	print("Custom tag: ", wordList[1])
+					if wordList[2] == "INDI":
+						id = re.sub('@', '', wordList[1])
+						print(id)
+						individuals[id] = {'NAME':'NA', 'SEX':'NA', 'BIRT':'NA', 'DEAT':'NA', 'CHIL':'NA', 'HUSB':'NA', 'WIFE':'NA', 'MARR':'NA'}
+						print(individuals)
+				
 				
 				# otherwise, invalid tag
 				else:
-					print("<-- ", wordList[0], "|", wordList[1], "|N|", remainder, sep ='')
+					print("Tag not of interest- skipping to next line")
+
+except IOError:
+	print("An error occured trying to access the data file.")
+
+except ImportError:
+	print("No module found.")	
+
+except:
+	print("An error occured.")
 				
 				
 
