232061e19c3eec3690e6544077f3c2992d4c36c6
Nick Cohron <dcohron@stevens.edu>
2017-06-05 11:15:56 -0400

    Project update
    Commit includes working individual parsing and data structure creation

--- Project #3.py
+++ Project #3.py
@@ -22,9 +22,23 @@
 # 2) GEDCOM tag full list:  http://wiki-en.genealogy.net/GEDCOM-Tags
 # 3) Error Handling:  http://www.pythonforbeginners.com/error-handling/python-try-and-except
 
-def getLine(dataList):
+### NOTE:
+# Data structure- 
+# individuals[individualID] = {'NAME':'NA', 'SEX':'NA', 'BIRT':'NA', 'DEAT':'NA'}
+# 	AGE and ALIVE are calculated fields 
+# 	CHIL, HUSB and WIFE are calculated from the "families" dictionary
+# families[familyID] = {'HUSB':'NA', 'WIFE':'NA', 'CHIL':'NA', 'MARR':'NA', 'DIV':'NA'}
+
+
+# function to get next line and process into list of words
+def getWords(dataList):
 	line = dataList.pop(0)
-	return line
+	# strip trailing whitespace
+	line = line.rstrip()
+			
+	# split line into words
+	wordList = line.split()
+	return wordList
 
 # put code into try/except for error handling
 try: 
@@ -40,11 +54,12 @@ try:
 	# List of valid GEDCOM tags
 	tagLong = ["ABBR", "ADDR", "ADR1", "ADR2", "ADOP", "AFN", "AGE", "AGNC", "ALIA", "ANCE", "ANCI", "ANUL", "ASSO", "AUTH", "BAPL", "BAPM", "BARM", "BASM", "BIRT", "BLES", "BLOB", "BURI", "CALN", "CAST", "CAUS", "CENS", "CHAN", "CHAR", "CHIL", "SHR", "CHRA", "CITY", "CONC", "CONF", "CONL", "CONT", "COPR", "CORP", "CREM", "CTRY", "DATA", "DATE", "DEAT", "DESC", "DESI", "DEST", "DIV", "DIVF", "DSCR", "EDUC", "EMAIL", "EMIG", "ENDL", "ENGA", "EVEN", "FACT", "FAM", "FAMC", "FAMF", "FAMS", "FAX", "FCOM", "FILE", "FONE", "FORM", "GEDC", "GIVN", "GRAD", "HEAD", "HUSB", "IDNO", "IMMI", "INDI", "LANG", "LATI", "LEGA", "LONG", "MAP", "MARB", "MARC", "MARL", "MARR", "MARS", "MEDI", "NAME", "NATI", "NATU", "NCHI", "NICK", "NMR", "NOTE", "NPFX", "NSFX", "OBJE", "OCCU", "ORDI", "ORDN", "PAGE", "PEDI", "PHON", "PLAC", "POST", "PROB", "PROP", "PUBL", "QUAY", "REFN", "RELA", "RELI", "REPO", "RESI", "RESN", "RETI", "RFN", "RIN", "ROLE", "ROMN", "SEX", "SLGC", "SLGS", "SOUR", "SPFX", "SSN", "STAE", "STAT", "SUBM", "SUBN", "SURN", "TEMP", "TEXT", "TIME", "TITL", "TRLR", "TYPE", "VERS", "WIFE", "WWW", "WILL"]
 	tagShort = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM", "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
-	tagInterest = ["NAME", "SEX", "BIRT", "DEAT", "CHIL", "HUSB", "WIFE", "MARR"]
+	tagInterest = ["NAME", "SEX", "BIRT", "DEAT"]
 	tagList = tagInterest
 	
 	# declare empty dictionary as primary data structure
 	individuals = {}
+	families = {}
 
 	# read in entire file into list of lines
 	f = open(path, 'r')
@@ -52,43 +67,64 @@ try:
 
 	# remove empty lines from list of lines
 	data = list(filter(None, data))
+	print(data)
 	
 		
-	for line in data:
-		print(line)
+	while data:
+		wordList = getWords(data)
+		print(wordList)
 		
 		# skips leading comments and any line that does not 
 		# start with a number
-		if line[0].isdigit():
-			# strip trailing whitespace
-			line = line.rstrip()
+		if wordList[0].isdigit():
 			
-			# split line into words
-			wordList = line.split()
-			print (wordList)
+			# print (wordList)
 			# print(len(wordList))
 			remainder = ''.join(str(w + " ") for w in wordList[2:])
-			# if word is in the list of tags, add to dictionary
+
+			# process individual tags
 			if wordList[0] == '1' and wordList[1] in tagList:
-				individuals[id][wordList[1]] = remainder
+				if wordList[1] == "NAME":
+					name = (wordList[2] + wordList[3])
+					individuals[individualID]["NAME"] = name
+					continue
+				elif wordList[1] == "BIRT":
+					nextLine = getWords(data)
+					date = (nextLine[2] + nextLine[3] + nextLine[4])
+					individuals[individualID]["BIRT"] = date
+					continue
+				elif wordList[1] == "DEAT":
+					nextLine = getWords(data)
+					date = (nextLine[2] + nextLine[3] + nextLine[4])
+					individuals[individualID]["DEAT"] = date
+					continue
+				else:
+					# only tag left is 'SEX'
+					individuals[individualID][wordList[1]] = wordList[2]
+					continue
 				
 			# if tag in word 3 is one of two special tags, handle it	
-			elif len(wordList) > 2 and (wordList[2] == "INDI" or wordList[2] == "FAM"):
+			elif (len(wordList) > 2) and ((wordList[2] == "INDI") or (wordList[2] == "FAM")):
 				if wordList[2] == "INDI":
 					individualID = re.sub('@', '', wordList[1])
-					print(individualID)
-					individuals[individualID] = {'NAME':'NA', 'SEX':'NA', 'BIRT':'NA', 'DEAT':'NA', 'CHIL':'NA', 'HUSB':'NA', 'WIFE':'NA', 'MARR':'NA'}
-					print(individuals)
+					individuals[individualID] = {'NAME':'NA', 'SEX':'NA', 'BIRT':'NA', 'DEAT':'NA'}
+					continue				
 				else:
-					famID = re.sub('@', '', wordList[1])
-					print("FamilyID= ", famID)	
+					familyID = re.sub('@', '', wordList[1])
+					print("FamilyID= ", familyID)	
+					families[familyID] = {'HUSB':'NA', 'WIFE':'NA', 'CHIL':'NA', 'MARR':'NA', 'DIV':'NA'}
+					continue
 			
 			# otherwise, invalid tag
 			else:
 				print("Tag not of interest- skipping to next line")
+				continue
 
 		else:
+			print("I do not know what")
 			continue
+
+	print(individuals)
 
 except IOError:
 	print("An error occured trying to access the data file.")
@@ -96,8 +132,8 @@ except IOError:
 except ImportError:
 	print("No module found.")	
 
-except:
-	print("An error occured.")
+# except:
+# 	print("An error occured.")
 				
 				
 
